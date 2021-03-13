bcc4b8cae5f4a53ab7fae8ddfdac677de5012b95
deonolarewaju <deonolarewaju@gmail.com>
2021-03-13 02:50:39 +0100

    play around

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
+import { PracticeComponentComponent } from './practice-component/practice-component.component'; 
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    PracticeComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  constructor() { }
+ allowNewServer = false;
+ serverCreationStatus = "No server was created";
+ serverName = 'TestServer';
+
+ serverStatus = 'offline'
+ serverId = 10
+
+
+  constructor() { 
+    this.serverStatus = Math.random() > 0.5 ? 'online':'offline';
+  }
 
   ngOnInit(): void {
   }
+
+  getServerStatus() {
+    this.serverStatus;
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created: server name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) { 
+    this.serverName = (<HTMLInputElement>event.target).value;
+  
+
+  }
+
 
 }
