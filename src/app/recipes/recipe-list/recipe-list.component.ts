393be3a7c6804e0fa69177458f7db27f3d699866
deonolarewaju <deonolarewaju@gmail.com>
2021-03-14 22:18:05 +0100

    added recipe model class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Just a test',
+      'https://www.hellomagazine.com/imagenes/cuisine/20210211106682/easy-chinese-new-year-recipes/1/0-514-361/chinese-new-year-recipes-food-t.jpg'),
+    new Recipe('A test recipe', 'Just a test',
+      'https://www.hellomagazine.com/imagenes/cuisine/20210211106682/easy-chinese-new-year-recipes/1/0-514-361/chinese-new-year-recipes-food-t.jpg'),
+    new Recipe('A test recipe', 'Just a test',
+      'https://www.hellomagazine.com/imagenes/cuisine/20210211106682/easy-chinese-new-year-recipes/1/0-514-361/chinese-new-year-recipes-food-t.jpg'),
+
+
+
+  ]
 
   constructor() { }
 
